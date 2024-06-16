8afd6cfdc3be9390df78219362930523ddec89d3
Ben Snaas <ben@wearebit.com>
2024-06-16 15:53:20 +0200

    .

--- self.py
+++ self.py
@@ -398,9 +398,9 @@ def train(input_bin="data/fineweb10B/fineweb_train_*.bin",
 
     quarter_depth = depth // 4
     batch_size_by_depth = {
-        quarter_depth: batch_size + 15,
-        2 * quarter_depth: batch_size + 10,
-        3 * quarter_depth: batch_size + 5,
+        quarter_depth: batch_size,
+        2 * quarter_depth: batch_size,
+        3 * quarter_depth: batch_size,
         depth: batch_size
     }
 
