27ff50f26646a12f4215a9a2cdef86fd797d6466
Ben Snaas <ben@wearebit.com>
2024-06-26 12:50:29 +0200

    .

--- progressive.py
+++ progressive.py
@@ -311,7 +311,7 @@ def train(input_bin="data/fineweb10B/fineweb_train_*.bin",
         return optimizer
 
     # progressive training schedule
-    progressive_schedule = [(6, 20000), (24, 80000)]
+    progressive_schedule = [(6, 10), (24, 10)]
 
     # Calculate total iterations in the progressive schedule
     total_scheduled_iters = sum(iters for _, iters in progressive_schedule)
