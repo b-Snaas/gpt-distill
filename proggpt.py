00bd9e47f3c5cada98e20588fd99629a56968690
Ben Snaas <ben@nosana.io>
2024-07-05 10:15:41 +0200

    .

--- proggpt.py
+++ proggpt.py
@@ -332,7 +332,7 @@ def train(input_bin="data/fineweb10B/fineweb_train_*.bin",
 
     # progressive training schedule
     progressive_schedule = [
-        (12, 10000, 45, 0.00020),
+        (12, 10000, 50, 0.00020),
         (48, 190000, 10, 0.00005),
         (3, 10000, 75, 0.00025),
         (6, 10000, 70, 0.00025),
