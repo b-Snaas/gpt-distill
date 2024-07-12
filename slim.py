67f00e21901848b642c7729583c9418da76e6554
Ben Snaas <ben@nosana.io>
2024-07-12 08:38:34 +0200

    .

--- slim.py
+++ slim.py
@@ -397,8 +397,8 @@ def train(input_bin="data/fineweb10B/fineweb_train_*.bin",
 
     # progressive training schedule
     progressive_schedule = [
-        (12, 16, 1024, 10000, 45, 0.0002),
-        (24, 16, 1024, 190000, 40, 0.0001)
+        (12, 16, 1024, 10000, 33, 0.0002),
+        (24, 16, 1024, 190000, 22, 0.0001)
     ]
 
     # Calculate total iterations in the progressive schedule
